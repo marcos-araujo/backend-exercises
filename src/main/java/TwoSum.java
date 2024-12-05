815a8b8dc0d51c63caedae62c535dcadad2ca5ae
marcos <marcos.txt@gmail.com>
2024-12-05 18:02:42 -0300

    TwoSum refac

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -3,12 +3,11 @@ import java.util.HashMap;
 
 public class TwoSum {
 
-    /*
-    Given an array of integers and an integer target, return indices of the two numbers such that they add up to target.
-    You may assume that each input would have exactly one solution, and you may not use the same element twice.
-    You can return the answer in any order.
-     */
-
+    /**
+      * Given an array of integers and an integer target, return indices of the two numbers such that they add up to target.
+      * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+      * You can return the answer in any order.
+      */
     public static void main(String[] args) {
         int[] array = {2, 7, 11, 15};
         int target = 9;
@@ -60,6 +59,7 @@ public class TwoSum {
      */
     private static int[] onePassHashTable(int[] nums, int target) {
         var hashMap = new HashMap<Integer, Integer>();
+
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (hashMap.containsKey(complement))
