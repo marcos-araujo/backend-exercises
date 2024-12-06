52913ce916b2f1a6a6d7d84bf104629b220e650c
Marcos Araujo <marcos.txt@gmail.com>
2024-12-06 12:33:57 -0300

    LengthOfLongestSubstring refac

--- src/main/java/LengthOfLongestSubstring.java
+++ src/main/java/LengthOfLongestSubstring.java
@@ -1,35 +1,64 @@
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class LengthOfLongestSubstring {
 
-    /*
-        Given a string s, find the length of the longest substring without repeating characters.
-    */
+    /**
+      * Given a string s, find the length of the longest substring without repeating characters.
+      */
 
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstringSet("pwwkew"));
+        String input = "pwwkew";
+        System.out.println("Map Solution: " + mapSolution(input));
+        System.out.println("Set Solution: " + setSolution(input));
     }
 
-    public static int lengthOfLongestSubstringSet(String s) {
-        var myHash = new HashSet<Character>();
-        int left = 0;
-        int length = 0;
+    /**
+     * Time Complexity: O(n)
+     */
+    public static int mapSolution(String s) {
+        int maxLength = 0;
+        Map<Character, Integer> charIndexMap = new HashMap<>();
+        int start = 0;
 
-        for (int right = 0; right < s.length(); right++) {
+        for (int end = 0; end < s.length(); end++) {
+            char currentChar = s.charAt(end);
 
-            if (!myHash.contains(s.charAt(right))) {
-                myHash.add(s.charAt(right));
-                length = Math.max(length, right - left + 1);
-            } else {
-                while (myHash.contains(s.charAt(right))) {
-                    myHash.remove(s.charAt(left));
-                    left++;
-                }
-                myHash.add(s.charAt(right));
-            }
+            if (charIndexMap.containsKey(currentChar))
+                start = Math.max(start, charIndexMap.get(currentChar) + 1);
+
+            charIndexMap.put(currentChar, end);
+
+            maxLength = Math.max(maxLength, end - start + 1);
         }
 
-        return length;
+        return maxLength;
+    }
+
+    /**
+     * Time Complexity: O(n)
+     */
+    public static int setSolution(String s) {
+        int n = s.length();
+        int maxLength = 0;
+        Set<Character> charSet = new HashSet<>();
+        int start = 0;
+
+        for (int end = 0; end < n; end++) {
+            char currentChar = s.charAt(end);
+
+            while (charSet.contains(currentChar)) {
+                charSet.remove(s.charAt(start));
+                start++;
+            }
+
+            charSet.add(currentChar);
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+
+        return maxLength;
     }
 
 }
